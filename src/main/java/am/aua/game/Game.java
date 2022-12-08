b38eae9f47c6df23a043af2e5f7664c223195146
senpie <smbo.voskanyan2002@gmail.com>
2022-12-08 18:53:55 +0400

    broken cube

--- src/main/java/am/aua/game/Game.java
+++ src/main/java/am/aua/game/Game.java
@@ -2,6 +2,9 @@ package am.aua.game;
 
 import am.aua.entities.Camera;
 import am.aua.entities.Light;
+import am.aua.models.Entity;
+import am.aua.models.RawModel;
+import am.aua.models.TexturedModel;
 import am.aua.rendererEngine.Loader;
 import am.aua.rendererEngine.Renderer;
 import am.aua.rendererEngine.Window;
@@ -22,6 +25,8 @@ public class Game implements ApplicationListener {
     private Light light;
     private Terrain terrain;
 
+    private Entity cube;
+
     @Override
     public void create() {
         Loader loader = new Loader();
@@ -31,6 +36,95 @@ public class Game implements ApplicationListener {
         camera = new Camera();
         light = new Light(new Vector3f(20000,20000,2000), new Vector3f(1,1,1));
         terrain = new Terrain(0,0, loader, new Texture(loader.loadTexture("grass.png")));
+
+        float[] vertices = {
+                -0.5f,0.5f,0,
+                -0.5f,-0.5f,0,
+                0.5f,-0.5f,0,
+                0.5f,0.5f,0,
+
+                -0.5f,0.5f,1,
+                -0.5f,-0.5f,1,
+                0.5f,-0.5f,1,
+                0.5f,0.5f,1,
+
+                0.5f,0.5f,0,
+                0.5f,-0.5f,0,
+                0.5f,-0.5f,1,
+                0.5f,0.5f,1,
+
+                -0.5f,0.5f,0,
+                -0.5f,-0.5f,0,
+                -0.5f,-0.5f,1,
+                -0.5f,0.5f,1,
+
+                -0.5f,0.5f,1,
+                -0.5f,0.5f,0,
+                0.5f,0.5f,0,
+                0.5f,0.5f,1,
+
+                -0.5f,-0.5f,1,
+                -0.5f,-0.5f,0,
+                0.5f,-0.5f,0,
+                0.5f,-0.5f,1
+        };
+
+        float[] textureCoords = {
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
+        };
+
+        float[] normals = {
+                0, 0, -1,
+                0, 0, 1,
+                0, 1, 0,
+                0, -1, 0,
+                1, 0, 0,
+                -1, 0, 0,
+        };
+
+        int[] indices = {
+                0,1,3,
+                3,1,2,
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
+        };
+
+        RawModel model = loader.loadToVAO(vertices, textureCoords, normals, indices);
+
+        TexturedModel staticModel = new TexturedModel(model, new Texture(loader.loadTexture("grass.png")));
+
+        cube = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
     }
 
     @Override
@@ -50,6 +144,7 @@ public class Game implements ApplicationListener {
     @Override
     public void render() {
         renderer.processTerrain(terrain);
+        renderer.processEntity(cube);
         renderer.render(light, camera);
     }
 
